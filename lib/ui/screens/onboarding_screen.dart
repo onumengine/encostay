eed67e38a1e9a28d23451a1296aa180360999e60
onumdev <onumdev@gmail.com>
2021-08-15 09:51:44 +0100

    create the onboarding screen skeleton

---
+++ lib/ui/screens/onboarding_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IntroductionScreen(),
+    );
+  }
+}
