dc3a7790e878067172cdd34e00226962a101d7f9
onumdev <onumdev@gmail.com>
2021-08-15 13:10:22 +0100

    create a test PageViewModel

--- lib/ui/screens/onboarding_screen.dart
+++ lib/ui/screens/onboarding_screen.dart
@@ -4,7 +4,17 @@ import 'package:introduction_screen/introduction_screen.dart';
 class OnboardingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IntroductionScreen(),
+      body: IntroductionScreen(
+        pages: <PageViewModel>[
+          PageViewModel(
+            title: 'Find Hundreds of hotels',
+            body:
+                'Discover hundreds of hotels that spread across the world for you',
+          ),
+          PageViewModel(),
+          PageViewModel(),
+        ],
+      ),
     );
   }
 }
