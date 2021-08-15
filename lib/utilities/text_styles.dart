695097056bf5ea1db29f4775040df34fc3398660
onumdev <onumdev@gmail.com>
2021-08-15 12:48:28 +0100

    Store all text styles in a separate file

--- lib/utilities/text_styles.dart
+++ lib/utilities/text_styles.dart
@@ -5,3 +5,45 @@ const semiBold24 = TextStyle(
   height: 34,
   letterSpacing: 0,
 );
+
+const medium20 = TextStyle(
+  fontSize: 20,
+  height: 30,
+  letterSpacing: 0,
+);
+
+const medium16 = TextStyle(
+  fontSize: 16,
+  height: 23,
+  letterSpacing: 0,
+);
+
+const regular16 = TextStyle(
+  fontSize: 16,
+  height: 25,
+  letterSpacing: 0,
+);
+
+const medium14 = TextStyle(
+  fontSize: 14,
+  height: 20,
+  letterSpacing: 0,
+);
+
+const regular14 = TextStyle(
+  fontSize: 14,
+  height: 22,
+  letterSpacing: 0,
+);
+
+const regular12 = TextStyle(
+  fontSize: 12,
+  height: 18,
+  letterSpacing: 0,
+);
+
+const regular11 = TextStyle(
+  fontSize: 11,
+  height: 18,
+  letterSpacing: 0,
+);
