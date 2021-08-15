3cc510825072580661c4eefc8a53bbc1e5efa1f1
onumdev <onumdev@gmail.com>
2021-08-15 12:54:45 +0100

    store all the brand colors in one file

---
+++ lib/utilities/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const brandOrange = Color(0xfffea401);
+const brandBrown = Color(0xff412525);
+const brandYellow = Color(0xfffece00);
+const brandLight = Color(0xfffff4e0);
+const brandLightGreen = Color(0xffe2f4f3);
+const brandWhite = Color(0xffffffff);
+const brandBackground = Color(0xfffafafa);
