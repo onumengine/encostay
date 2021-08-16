fa8b6ab84e039b28bd96116f363bff10443af232
onumdev <onumdev@gmail.com>
2021-08-16 21:00:53 +0100

    add the three onboarding pages

--- lib/ui/screens/onboarding_screen.dart
+++ lib/ui/screens/onboarding_screen.dart
@@ -30,12 +30,63 @@ class OnboardingScreen extends StatelessWidget {
                 alignment: Alignment.centerLeft,
                 child: Text(
                   'You can now experience the next level travel experience for hotel bookings',
+                  style: regular14,
                 ),
               ),
               image: Align(
                 alignment: Alignment.topRight,
                 child: Image.asset(
                   'lib/assets/images/onboarding_1.png',
+                  width: constraints.widthConstraints().maxWidth * 3 / 4,
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            PageViewModel(
+              titleWidget: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Find Hundreds of hotels',
+                  textAlign: TextAlign.start,
+                  style: semiBold24,
+                ),
+              ),
+              bodyWidget: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Discover hundreds of hotels that spread across the world for you',
+                  style: regular14,
+                ),
+              ),
+              image: Align(
+                alignment: Alignment.topRight,
+                child: Image.asset(
+                  'lib/assets/images/onboarding_2.png',
+                  width: constraints.widthConstraints().maxWidth * 3 / 4,
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            PageViewModel(
+              titleWidget: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Let\'s discover the world',
+                  textAlign: TextAlign.start,
+                  style: semiBold24,
+                ),
+              ),
+              bodyWidget: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Book your hotel now for the next level travel experience. Enjoy your trip',
+                  style: regular14,
+                ),
+              ),
+              image: Align(
+                alignment: Alignment.topRight,
+                child: Image.asset(
+                  'lib/assets/images/onboarding_3.png',
                   width: constraints.widthConstraints().maxWidth * 3 / 4,
                   fit: BoxFit.fill,
                 ),
