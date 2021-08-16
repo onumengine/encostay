fd1856cfd5a8751c4652806b6349cd0928475eb6
onumdev <onumdev@gmail.com>
2021-08-16 17:17:10 +0100

    align the first onboarding page image

--- lib/ui/screens/onboarding_screen.dart
+++ lib/ui/screens/onboarding_screen.dart
@@ -4,23 +4,33 @@ import 'package:introduction_screen/introduction_screen.dart';
 class OnboardingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IntroductionScreen(
-        done: Text("Get Started"),
-        onDone: () {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text("Clicked done"),
+      body: LayoutBuilder(
+        builder: (_, constraints) => IntroductionScreen(
+          done: Text("Get Started"),
+          onDone: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text("Clicked done"),
+              ),
+            );
+          },
+          showNextButton: false,
+          pages: <PageViewModel>[
+            PageViewModel(
+              title: 'Find Hundreds of hotels',
+              body:
+                  'Discover hundreds of hotels that spread across the world for you',
+              image: Align(
+                alignment: Alignment.topRight,
+                child: Image.asset(
+                  'lib/assets/images/onboarding_1.png',
+                  width: constraints.widthConstraints().maxWidth * 3 / 4,
+                  fit: BoxFit.fill,
+                ),
+              ),
             ),
-          );
-        },
-        showNextButton: false,
-        pages: <PageViewModel>[
-          PageViewModel(
-            title: 'Find Hundreds of hotels',
-            body:
-                'Discover hundreds of hotels that spread across the world for you',
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
