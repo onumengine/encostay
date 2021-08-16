9c2a58937918f457340d952905a4dcb43ad922d1
onumdev <onumdev@gmail.com>
2021-08-16 18:12:17 +0100

    refactor all the text style line heights

--- lib/utilities/text_styles.dart
+++ lib/utilities/text_styles.dart
@@ -2,49 +2,49 @@ import 'package:flutter/material.dart';
 
 const semiBold24 = TextStyle(
   fontSize: 24,
-  height: 1.416,
+  height: 1.42,
   letterSpacing: 0,
   fontWeight: FontWeight.w600,
 );
 
 const medium20 = TextStyle(
   fontSize: 20,
-  height: 30,
+  height: 1.5,
   letterSpacing: 0,
 );
 
 const medium16 = TextStyle(
   fontSize: 16,
-  height: 23,
+  height: 1.44,
   letterSpacing: 0,
 );
 
 const regular16 = TextStyle(
   fontSize: 16,
-  height: 25,
+  height: 1.56,
   letterSpacing: 0,
 );
 
 const medium14 = TextStyle(
   fontSize: 14,
-  height: 20,
+  height: 1.43,
   letterSpacing: 0,
 );
 
 const regular14 = TextStyle(
   fontSize: 14,
-  height: 22,
+  height: 1.57,
   letterSpacing: 0,
 );
 
 const regular12 = TextStyle(
   fontSize: 12,
-  height: 18,
+  height: 1.5,
   letterSpacing: 0,
 );
 
 const regular11 = TextStyle(
   fontSize: 11,
-  height: 18,
+  height: 1.64,
   letterSpacing: 0,
 );
