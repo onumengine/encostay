ddb0f465883fcdc7c008f4b806aec8a4cfadf6b1
onumdev <onumdev@gmail.com>
2021-08-16 13:05:26 +0100

    set Montserrat as the default font

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'Montserrat',
       ),
       home: OnboardingScreen(),
     );
