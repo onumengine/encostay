5d8f3333895651740ae79d64de1c3937431e86bb
onumengine <onumdev@gmail.com>
2021-08-23 03:50:39 -0700

    paint the circular overlay with a custom painter

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -11,9 +11,26 @@ class SplashScreen extends StatelessWidget {
         child: Stack(
           children: <Widget>[
             Image(image: AssetImage('lib/assets/images/splash.png'),),
+            CustomPaint(
+              painter: CircularOverlay(screenSize: MediaQuery.of(context).size),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+class CircularOverlay extends CustomPainter {
+  Size screenSize;
+  CircularOverlay({this.screenSize = const Size(0, 0)});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paintBrush = Paint()..color = brandOverlay..style = PaintingStyle.fill;
+    canvas.drawCircle(Offset((screenSize.width * 0.06), (screenSize.height * 0.78)), (screenSize.width * 1.02), paintBrush);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
