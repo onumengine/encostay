5a9046d326318271677bc97ac13aa4cc9b9d530d
onumengine <onumdev@gmail.com>
2021-08-24 17:50:33 -0700

    rearrange the text styles

--- lib/utilities/text_styles.dart
+++ lib/utilities/text_styles.dart
@@ -9,35 +9,10 @@ const bold20 = TextStyle(
   height: 2.45,
 );
 
-const semiBold24 = TextStyle(
-  fontSize: 24,
-  height: 1.42,
-  letterSpacing: 0,
-  fontWeight: FontWeight.w600,
-);
-
 const medium13 = TextStyle(
   fontSize: 13,
   fontWeight: FontWeight.w500,
   height: 3.77,
-);
-
-const medium20 = TextStyle(
-  fontSize: 20,
-  height: 1.5,
-  letterSpacing: 0,
-);
-
-const medium16 = TextStyle(
-  fontSize: 16,
-  height: 1.44,
-  letterSpacing: 0,
-);
-
-const regular16 = TextStyle(
-  fontSize: 16,
-  height: 1.56,
-  letterSpacing: 0,
 );
 
 const medium14 = TextStyle(
@@ -46,14 +21,21 @@ const medium14 = TextStyle(
   letterSpacing: 0,
 );
 
-const regular14 = TextStyle(
-  fontSize: 14,
-  height: 1.57,
+const medium15 = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontSize: 15,
+  height: 1.63,
+  color: brandBrown,
+);
+
+const medium16 = TextStyle(
+  fontSize: 16,
+  height: 1.44,
   letterSpacing: 0,
 );
 
-const regular12 = TextStyle(
-  fontSize: 12,
+const medium20 = TextStyle(
+  fontSize: 20,
   height: 1.5,
   letterSpacing: 0,
 );
@@ -64,16 +46,34 @@ const regular11 = TextStyle(
   letterSpacing: 0,
 );
 
+const regular12 = TextStyle(
+  fontSize: 12,
+  height: 1.5,
+  letterSpacing: 0,
+);
+
+const regular14 = TextStyle(
+  fontSize: 14,
+  height: 1.57,
+  letterSpacing: 0,
+);
+
+const regular16 = TextStyle(
+  fontSize: 16,
+  height: 1.56,
+  letterSpacing: 0,
+);
+
+const semiBold24 = TextStyle(
+  fontSize: 24,
+  height: 1.42,
+  letterSpacing: 0,
+  fontWeight: FontWeight.w600,
+);
+
 const splashTitle = TextStyle(
   fontWeight: FontWeight.w900,
   fontSize: 30,
   color: brandBrown,
   height: 1.63,
-);
-
-const medium15 = TextStyle(
-  fontWeight: FontWeight.w500,
-  fontSize: 15,
-  height: 1.63,
-  color: brandBrown,
 );
