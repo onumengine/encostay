3dafb1d3ef0f6576870657cc813bf85febc4e624
onumengine <onumdev@gmail.com>
2021-08-24 18:05:50 -0700

    create the login form

---
+++ lib/ui/organisms/login_form.dart
@@ -0,0 +1,14 @@
+import 'package:encostay/utilities/colors.dart';
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatefulWidget {
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 25),
+    );
+  }
+}
