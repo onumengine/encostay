3755a4d32bd8501ead956e0424194d01ddda38fd
onumengine <onumdev@gmail.com>
2021-08-26 23:11:50 +0100

    create the sign up form

---
+++ lib/ui/organisms/signup_form.dart
@@ -0,0 +1,55 @@
+import 'package:encostay/ui/molecules/text_widget.dart';
+import 'package:encostay/utilities/text_styles.dart';
+import 'package:flutter/material.dart';
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({Key? key}) : super(key: key);
+
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  GlobalKey _firstNameKey = GlobalKey();
+  GlobalKey _lastNameKey = GlobalKey();
+  GlobalKey _birthdayKey = GlobalKey();
+  GlobalKey _emailKey = GlobalKey();
+  TextEditingController _firstNameController = TextEditingController();
+  TextEditingController _lastNameController = TextEditingController();
+  TextEditingController _birthdayController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 25),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Align(
+            alignment: AlignmentDirectional.centerStart,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20) +
+                  EdgeInsets.only(bottom: (screenSize.height / 101.5)),
+              child: Text(
+                'First Name',
+                style: TextStyle(
+                  fontSize: medium14.fontSize,
+                  fontWeight: medium14.fontWeight,
+                  height: 1.64,
+                ),
+              ),
+            ),
+          ),
+          TextWidget(
+            key: _firstNameKey,
+            hintText: 'Enter your first name',
+            controller: _firstNameController,
+          ),
+          SizedBox(height: (screenSize.height / 45)),
+        ],
+      ),
+    );
+  }
+}
