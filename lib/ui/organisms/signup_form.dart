ad9d77d6648b0835299a37c26c63885c0ed158a8
onumengine <onumdev@gmail.com>
2021-08-27 13:56:52 +0100

    wrap the root column in a single child scrollview

--- lib/ui/organisms/signup_form.dart
+++ lib/ui/organisms/signup_form.dart
@@ -26,148 +26,154 @@ class _SignUpFormState extends State<SignUpForm> {
     Size screenSize = MediaQuery.of(context).size;
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 25),
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Align(
-            alignment: AlignmentDirectional.centerStart,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20) +
-                  EdgeInsets.only(bottom: (screenSize.height / 101.5)),
-              child: Text(
-                'First Name',
-                style: TextStyle(
-                  fontSize: medium14.fontSize,
-                  fontWeight: medium14.fontWeight,
-                  height: 1.64,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Align(
+              alignment: AlignmentDirectional.centerStart,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20) +
+                    EdgeInsets.only(bottom: (screenSize.height / 101.5)),
+                child: Text(
+                  'First Name',
+                  style: TextStyle(
+                    fontSize: medium14.fontSize,
+                    fontWeight: medium14.fontWeight,
+                    height: 1.64,
+                  ),
                 ),
               ),
             ),
-          ),
-          TextWidget(
-            key: _firstNameKey,
-            hintText: 'Enter your first name',
-            controller: _firstNameController,
-          ),
-          SizedBox(height: (screenSize.height / 45)),
-          Align(
-            alignment: AlignmentDirectional.centerStart,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20) +
-                  EdgeInsets.only(bottom: (screenSize.height / 101.5)),
-              child: Text(
-                'Last Name',
-                style: TextStyle(
-                  fontSize: medium14.fontSize,
-                  fontWeight: medium14.fontWeight,
-                  height: 1.64,
+            TextWidget(
+              key: _firstNameKey,
+              hintText: 'Enter your first name',
+              controller: _firstNameController,
+            ),
+            SizedBox(height: (screenSize.height / 45)),
+            Align(
+              alignment: AlignmentDirectional.centerStart,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20) +
+                    EdgeInsets.only(bottom: (screenSize.height / 101.5)),
+                child: Text(
+                  'Last Name',
+                  style: TextStyle(
+                    fontSize: medium14.fontSize,
+                    fontWeight: medium14.fontWeight,
+                    height: 1.64,
+                  ),
                 ),
               ),
             ),
-          ),
-          TextWidget(
-            key: _lastNameKey,
-            hintText: 'Enter your last name',
-            controller: _lastNameController,
-          ),
-          SizedBox(height: (screenSize.height / 162.4)),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Text(
-              'Make sure it matches the name on your government ID',
-              style: normal10,
+            TextWidget(
+              key: _lastNameKey,
+              hintText: 'Enter your last name',
+              controller: _lastNameController,
             ),
-          ),
-          SizedBox(
-            child: Divider(
-              thickness: 1,
-              color: brandOrange,
-            ),
-          ),
-          SizedBox(height: (screenSize.height / 64.46)),
-          Align(
-            alignment: AlignmentDirectional.centerStart,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20) +
-                  EdgeInsets.only(bottom: (screenSize.height / 101.5)),
+            SizedBox(height: (screenSize.height / 162.4)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Text(
-                'Birthday',
-                style: TextStyle(
-                  fontSize: medium14.fontSize,
-                  fontWeight: medium14.fontWeight,
-                  height: 1.64,
+                'Make sure it matches the name on your government ID',
+                style: normal10,
+              ),
+            ),
+            SizedBox(height: (screenSize.height / 45)),
+            SizedBox(
+              child: Divider(
+                thickness: 1,
+                color: brandOrange,
+              ),
+            ),
+            SizedBox(height: (screenSize.height / 64.46)),
+            Align(
+              alignment: AlignmentDirectional.centerStart,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20) +
+                    EdgeInsets.only(bottom: (screenSize.height / 101.5)),
+                child: Text(
+                  'Birthday',
+                  style: TextStyle(
+                    fontSize: medium14.fontSize,
+                    fontWeight: medium14.fontWeight,
+                    height: 1.64,
+                  ),
                 ),
               ),
             ),
-          ),
-          Container(
-            height: 43,
-            decoration: BoxDecoration(
-              color: brandWhite,
-              borderRadius: BorderRadius.all(
-                Radius.circular(27.5),
-              ),
-            ),
-            child: Material(
-              color: Colors.transparent,
-              child: InkWell(
-                splashColor: brandWhite.withOpacity(0.5),
-                borderRadius: BorderRadius.circular(28),
-                onTap: () {},
-              ),
-            ),
-          ),
-          SizedBox(height: (screenSize.height / 162.4)),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 49),
-            child: Text(
-              'To sign up, you need to be at least 18. Other people who use Encostay won\'t see your birthday.',
-              style: normal10,
-            ),
-          ),
-          Align(
-            alignment: AlignmentDirectional.centerStart,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20) +
-                  EdgeInsets.only(bottom: (screenSize.height / 101.5)),
-              child: Text(
-                'Email',
-                style: TextStyle(
-                  fontSize: medium14.fontSize,
-                  fontWeight: medium14.fontWeight,
-                  height: 1.64,
-                ),
-              ),
-            ),
-          ),
-          TextWidget(
-            key: _emailKey,
-            hintText: 'Enter your username or E-mail',
-            controller: _emailController,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16, right: 13),
-            child: Text(
-              'By selecting Agree and continue below, I agree to Airbnb\'s Terms of Service, Payments Terms of Service, Privacy Policy, and Nondiscrimination Policy.',
-              style: normal10,
-            ),
-          ),
-          BrandButton(
-            child: Text(
-              'Agree and continue',
-              style: TextStyle(
-                fontSize: bold18.fontSize,
-                fontWeight: bold18.fontWeight,
-                height: bold18.height,
+            Container(
+              height: 43,
+              decoration: BoxDecoration(
                 color: brandWhite,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(27.5),
+                ),
+              ),
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  splashColor: brandWhite.withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(28),
+                  onTap: () {},
+                ),
               ),
             ),
-            color: brandOrange,
-            height: 43,
-            onTap: () {},
-          ),
-        ],
+            SizedBox(height: (screenSize.height / 162.4)),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 49),
+              child: Text(
+                'To sign up, you need to be at least 18. Other people who use Encostay won\'t see your birthday.',
+                style: normal10,
+              ),
+            ),
+            SizedBox(height: (screenSize.height / 47.76)),
+            Align(
+              alignment: AlignmentDirectional.centerStart,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20) +
+                    EdgeInsets.only(bottom: (screenSize.height / 101.5)),
+                child: Text(
+                  'Email',
+                  style: TextStyle(
+                    fontSize: medium14.fontSize,
+                    fontWeight: medium14.fontWeight,
+                    height: 1.64,
+                  ),
+                ),
+              ),
+            ),
+            TextWidget(
+              key: _emailKey,
+              hintText: 'Enter your username or E-mail',
+              controller: _emailController,
+            ),
+            SizedBox(height: (screenSize.height / 23.2)),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 13),
+              child: Text(
+                'By selecting Agree and continue below, I agree to Airbnb\'s Terms of Service, Payments Terms of Service, Privacy Policy, and Nondiscrimination Policy.',
+                style: normal10,
+              ),
+            ),
+            SizedBox(height: (screenSize.height / 47.76)),
+            BrandButton(
+              child: Text(
+                'Agree and continue',
+                style: TextStyle(
+                  fontSize: bold18.fontSize,
+                  fontWeight: bold18.fontWeight,
+                  height: bold18.height,
+                  color: brandWhite,
+                ),
+              ),
+              color: brandOrange,
+              height: 43,
+              onTap: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
