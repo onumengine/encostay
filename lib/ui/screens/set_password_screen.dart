3244433dd7f48d2fb2bcf80ae08df8c5210749bb
onumengine <onumdev@gmail.com>
2021-08-28 16:28:35 +0100

    add instructional text below the confirm password text

--- lib/ui/screens/set_password_screen.dart
+++ lib/ui/screens/set_password_screen.dart
@@ -1,3 +1,5 @@
+import 'package:encostay/ui/molecules/text_widget.dart';
+import 'package:encostay/utilities/text_styles.dart';
 import 'package:flutter/material.dart';
 
 class SetPasswordScreen extends StatefulWidget {
@@ -8,8 +10,15 @@ class SetPasswordScreen extends StatefulWidget {
 }
 
 class _SetPasswordScreenState extends State<SetPasswordScreen> {
+  GlobalKey _createPasswordKey = GlobalKey();
+  GlobalKey _confirmPasswordKey = GlobalKey();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _confirmPasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -18,7 +27,46 @@ class _SetPasswordScreenState extends State<SetPasswordScreen> {
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
+              SizedBox(height: (screenHeight / 23.2)),
               Text('Set Password'),
+              SizedBox(height: (screenHeight / 29)),
+              Align(
+                alignment: AlignmentDirectional.centerStart,
+                child: Text(
+                  'Create Password',
+                  style: TextStyle(
+                    fontSize: medium14.fontSize,
+                    fontWeight: medium14.fontWeight,
+                    height: 1.64,
+                  ),
+                ),
+              ),
+              TextWidget(
+                key: _createPasswordKey,
+                hintText: 'New Password',
+                controller: _passwordController,
+              ),
+              SizedBox(
+                height: (screenHeight / 58),
+              ),
+              Align(
+                alignment: AlignmentDirectional.centerStart,
+                child: Text(
+                  'Create Password',
+                  style: TextStyle(
+                    fontSize: medium14.fontSize,
+                    fontWeight: medium14.fontWeight,
+                    height: 1.64,
+                  ),
+                ),
+              ),
+              TextWidget(
+                key: _confirmPasswordKey,
+                hintText: 'New Password',
+                controller: _confirmPasswordController,
+              ),
+              Text(
+                  'Lorem ipsum dolor sit amet consectetur adipiscing elit. Suspendisse ipsum leo molestie in arcu sapien'),
             ],
           ),
         ),
