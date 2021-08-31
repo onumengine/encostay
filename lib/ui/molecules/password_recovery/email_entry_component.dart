0a1dd94ccfd18be3b7e5d405f7632cd91f209d02
onumengine <onumdev@gmail.com>
2021-08-31 08:29:47 +0100

    create an email entry component for use in the modal bottom sheet

---
+++ lib/ui/molecules/password_recovery/email_entry_component.dart
@@ -0,0 +1,42 @@
+import 'dart:html';
+
+import 'package:encostay/ui/atoms/brand_button.dart';
+import 'package:encostay/ui/atoms/text_widget.dart';
+import 'package:flutter/material.dart';
+
+class EmailEntryComponent extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final GlobalKey _emailKey = GlobalKey();
+
+  EmailEntryComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Text('Forgot password'),
+        Text(
+            'Enter your email for the verification preocess. We will send a 4-digit code to your email'),
+        Text('Email'),
+        TextWidget(
+          key: _emailKey,
+          hintText: 'Enter your email',
+          controller: _emailController,
+        ),
+        BrandButton(
+          height: 57,
+          child: Text('Continue'),
+          onTap: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Email submitted'),
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
