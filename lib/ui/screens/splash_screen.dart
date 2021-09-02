9c262d449a03ba16a5cbbf96bd102966b5d2cf00
onumengine <onumdev@gmail.com>
2021-09-02 14:31:56 +0100

    fix whitespace at the sides of the splash image

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -7,84 +7,88 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        height: screenSize.height,
-        width: screenSize.width,
-        child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            Image(
-              image: AssetImage('lib/assets/images/splash.png'),
-            ),
-            Positioned(
-              top: 0,
-              left: 0,
-              child: CustomPaint(
-                painter: CircularOverlay(screenSize: screenSize),
+      body: LayoutBuilder(
+        builder: (context, constraints) => Container(
+          height: screenSize.height,
+          width: screenSize.width,
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              Image(
+                image: AssetImage('lib/assets/images/splash.png'),
+                width: screenSize.width,
+                fit: BoxFit.cover,
               ),
-            ),
-            Positioned(
-              top: (screenSize.height / 2.3),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Stack(
-                    alignment: Alignment.center,
-                    children: <Widget>[
-                      Image(
-                        image: AssetImage('lib/assets/images/icon_bg.png'),
-                        width: 123.46,
-                      ),
-                      Image(
-                        image: AssetImage('lib/assets/images/icon.png'),
-                        width: 50,
-                        height: 60,
-                      ),
-                    ],
-                  ),
-                  Text(
-                    'encostay',
-                    style: splashTitle,
-                  ),
-                  Text('Enjoy convenient stay.', style: medium15),
-                  SizedBox(
-                    height: (screenSize.height * 0.09),
-                  ),
-                  InkResponse(
-                    onTap: () {
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(
-                          builder: (context) => OnboardingScreen(),
+              Positioned(
+                top: 0,
+                left: 0,
+                child: CustomPaint(
+                  painter: CircularOverlay(screenSize: screenSize),
+                ),
+              ),
+              Positioned(
+                top: (screenSize.height / 2.3),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Stack(
+                      alignment: Alignment.center,
+                      children: <Widget>[
+                        Image(
+                          image: AssetImage('lib/assets/images/icon_bg.png'),
+                          width: 123.46,
                         ),
-                      );
-                    },
-                    splashColor: brandWhite,
-                    child: Container(
-                      margin: EdgeInsets.symmetric(
-                          horizontal: (screenSize.width / 3.6)),
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 36, vertical: 18),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(29),
+                        Image(
+                          image: AssetImage('lib/assets/images/icon.png'),
+                          width: 50,
+                          height: 60,
                         ),
-                        color: brandBrown,
-                      ),
-                      child: Center(
-                        child: Text('Get started',
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500,
-                              color: brandWhite,
-                            )),
+                      ],
+                    ),
+                    Text(
+                      'encostay',
+                      style: splashTitle,
+                    ),
+                    Text('Enjoy convenient stay.', style: medium15),
+                    SizedBox(
+                      height: (screenSize.height * 0.09),
+                    ),
+                    InkResponse(
+                      onTap: () {
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (context) => OnboardingScreen(),
+                          ),
+                        );
+                      },
+                      splashColor: brandWhite,
+                      child: Container(
+                        margin: EdgeInsets.symmetric(
+                            horizontal: (screenSize.width / 3.6)),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(29),
+                          ),
+                          color: brandBrown,
+                        ),
+                        child: Center(
+                          child: Text('Get started',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                color: brandWhite,
+                              )),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            )
-          ],
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
