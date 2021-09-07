785013dc962246441649213e15575a437c7f92bc
onumengine <onumdev@gmail.com>
2021-09-07 21:48:50 +0100

    create a search box

---
+++ lib/ui/atoms/search_box.dart
@@ -0,0 +1,63 @@
+import 'package:encostay/ui/atoms/filters_button.dart';
+import 'package:encostay/utilities/colors.dart';
+import 'package:flutter/material.dart';
+
+class SearchBox extends StatelessWidget {
+  final TextEditingController? controller;
+
+  SearchBox({Key? key, required this.controller}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 47,
+      decoration: BoxDecoration(
+        color: brandWhite,
+        borderRadius: BorderRadius.all(
+          Radius.circular(13),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: brandTextInputShadow.withOpacity(0.13),
+          ),
+          BoxShadow(
+            color: brandTextInputShadow.withOpacity(0.13),
+          ),
+          BoxShadow(
+            color: brandTextInputShadow.withOpacity(0.13),
+          ),
+          BoxShadow(
+            color: brandTextInputShadow.withOpacity(0.13),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: ImageIcon(
+              AssetImage('lib/assets/images/search_icon.png'),
+              size: 24,
+              color: Color(0xffd8d8d8),
+            ),
+          ),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration.collapsed(
+                hintText: 'Search',
+                hintStyle: TextStyle(
+                  color: Color(0xffd8d8d8),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            child: FiltersButton(),
+          ),
+        ],
+      ),
+    );
+  }
+}
