a3d583d6a30f60d670d6bb9a5d70015068835b2f
onumengine <onumdev@gmail.com>
2021-09-08 13:50:04 +0100

    clipped the edges of the apartment images

--- lib/ui/molecules/apartment_card.dart
+++ lib/ui/molecules/apartment_card.dart
@@ -11,13 +11,38 @@ class ApartmentCard extends StatefulWidget {
 class _ApartmentCardState extends State<ApartmentCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 331.6,
-      width: MediaQuery.of(context).size.width / 1.74,
-      decoration: BoxDecoration(
-        color: brandWhite,
-        borderRadius: BorderRadius.all(
-          Radius.circular(20),
+    return AspectRatio(
+      aspectRatio: 0.65,
+      child: Container(
+        width: MediaQuery.of(context).size.width / 1.74,
+        padding: EdgeInsets.fromLTRB(12, 11, 13, 20),
+        decoration: BoxDecoration(
+          color: brandWhite,
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ),
+        ),
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 0.872,
+              child: Stack(
+                alignment: AlignmentDirectional.center,
+                fit: StackFit.expand,
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image(
+                      image: AssetImage('lib/assets/images/apartment_pic.png'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(),
+            Text(''),
+            Row(),
+          ],
         ),
       ),
     );
