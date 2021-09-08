7c88a1c0191bdeba171692a7352dcfaeeab1ea3b
onumengine <onumdev@gmail.com>
2021-09-08 05:11:09 +0100

    convert the CustomScrollView to a ListView

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:encostay/ui/atoms/filter_chip.dart';
 import 'package:encostay/ui/atoms/search_box.dart';
 import 'package:encostay/utilities/colors.dart';
 import 'package:encostay/utilities/text_styles.dart';
@@ -19,58 +20,68 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: brandBackgroundApp,
       body: SafeArea(
-        child: CustomScrollView(
-          slivers: [
-            SliverList(
-              delegate: SliverChildListDelegate([
-                SizedBox(
-                  height: kToolbarHeight,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 18, right: 28),
-                  child: Row(
+        child: ListView(
+          children: [
+            SizedBox(
+              height: kToolbarHeight,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 18, right: 28),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
                     mainAxisSize: MainAxisSize.max,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Column(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Hey Olaitan!',
-                            style: TextStyle(
-                              fontSize: medium14.fontSize,
-                              fontWeight: medium14.fontWeight,
-                              height: 23 / 14,
-                              color: Color(0xffaeafae),
-                            ),
-                          ),
-                          Text(
-                            'let\'s find your best residence!',
-                            style: TextStyle(
-                              fontSize: medium14.fontSize,
-                              fontWeight: medium14.fontWeight,
-                              height: 23 / 14,
-                            ),
-                          ),
-                        ],
+                      Text(
+                        'Hey Olaitan!',
+                        style: TextStyle(
+                          fontSize: medium14.fontSize,
+                          fontWeight: medium14.fontWeight,
+                          height: 23 / 14,
+                          color: Color(0xffaeafae),
+                        ),
                       ),
-                      CircleAvatar(
-                        backgroundImage:
-                            AssetImage('lib/assets/images/avatar.png'),
-                        radius: 24,
+                      Text(
+                        'let\'s find your best residence!',
+                        style: TextStyle(
+                          fontSize: medium14.fontSize,
+                          fontWeight: medium14.fontWeight,
+                          height: 23 / 14,
+                        ),
                       ),
                     ],
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 27, 25, 27),
-                  child: SearchBox(
-                    controller: controller,
+                  CircleAvatar(
+                    backgroundImage: AssetImage('lib/assets/images/avatar.png'),
+                    radius: 24,
                   ),
-                )
-              ]),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 27, 25, 27),
+              child: SearchBox(
+                controller: controller,
+              ),
+            ),
+            Container(
+              height: 35,
+              padding: EdgeInsets.only(left: 17),
+              child: ListView.separated(
+                itemBuilder: (_, index) => CustomChip(label: 'label'),
+                itemCount: 20,
+                separatorBuilder: (_, index) => SizedBox(
+                  width: 11,
+                ),
+                scrollDirection: Axis.horizontal,
+              ),
+            ),
+            SizedBox(
+              height: 25,
             ),
           ],
         ),
