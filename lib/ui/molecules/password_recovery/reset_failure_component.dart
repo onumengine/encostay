dcf0be38fe81b92edb9d77e0127029a58f550bc9
onumengine <onumdev@gmail.com>
2021-09-09 09:48:36 +0100

    integrate the reset failure component

---
+++ lib/ui/molecules/password_recovery/reset_failure_component.dart
@@ -0,0 +1,69 @@
+import 'package:encostay/ui/atoms/brand_button.dart';
+import 'package:encostay/utilities/colors.dart';
+import 'package:encostay/utilities/text_styles.dart';
+import 'package:flutter/material.dart';
+
+class ResetFailureComponent extends StatelessWidget {
+  const ResetFailureComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 74,
+        ),
+        Image.asset('lib/assets/images/password_reset_failure.png'),
+        SizedBox(
+          height: 28,
+        ),
+        Align(
+          alignment: AlignmentDirectional.centerStart,
+          child: Text(
+            'Password Reset Failed',
+            style: medium18BrownDark,
+          ),
+        ),
+        SizedBox(
+          height: 13,
+        ),
+        Align(
+          alignment: AlignmentDirectional.centerStart,
+          child: Text(
+            'Set the new password for your account so you can login and access all the features',
+            style: instructionalTextStyleModal,
+          ),
+        ),
+        SizedBox(
+          height: 41,
+        ),
+        BrandButton(
+          height: 57,
+          color: brandOrange,
+          child: Text(
+            'Retry',
+            style: TextStyle(
+              fontSize: bold18.fontSize,
+              fontWeight: bold18.fontWeight,
+              height: bold18.height,
+              color: brandWhite,
+            ),
+          ),
+          onTap: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Retrying password reset'),
+              ),
+            );
+          },
+        ),
+        SizedBox(
+          height: 64,
+        ),
+      ],
+    );
+  }
+}
