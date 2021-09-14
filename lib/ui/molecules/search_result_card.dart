a795981713b5b4c834b8395a5a3928bf276e617e
onumengine <onumdev@gmail.com>
2021-09-14 14:27:33 +0100

    open the apartment details screen on search result card tap

--- lib/ui/molecules/search_result_card.dart
+++ lib/ui/molecules/search_result_card.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:encostay/ui/atoms/flat_iconbutton.dart';
+import 'package:encostay/ui/screens/apartment_details_screen.dart';
 import 'package:encostay/utilities/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -16,216 +17,225 @@ class _SearchResultCardState extends State<SearchResultCard> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.maxFinite,
-      padding: EdgeInsets.fromLTRB(17, 14, 15, 15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: brandWhite,
-      ),
-      child: Column(
-        children: <Widget>[
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: <Widget>[
-              AspectRatio(
-                aspectRatio: 300 / 218.6,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(16),
-                  child: Image(
-                    image: AssetImage('lib/assets/images/apartment_pic.png'),
-                    fit: BoxFit.fitWidth,
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 15,
-                left: 12,
-                child: Container(
-                  height: 27,
-                  width: 47.5,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(24),
-                    color: Color(0x20ffffff),
-                  ),
-                  child: Stack(
-                    alignment: AlignmentDirectional.center,
-                    children: [
-                      ImageFiltered(
-                        imageFilter: ImageFilter.blur(sigmaX: 3.0),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          ImageIcon(
-                            AssetImage('lib/assets/images/rating_icon.png'),
-                            size: 18,
-                            color: brandYellow,
-                          ),
-                          Text(
-                            '4.0',
-                            style: TextStyle(
-                              fontSize: 8,
-                              fontWeight: FontWeight.w700,
-                              height: 18 / 8,
-                              color: brandWhite,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Positioned(
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _isFavorited = !_isFavorited;
-                    });
-                  },
-                  child: ImageIcon(
-                    AssetImage('lib/assets/images/favorite_icon.png'),
-                    color: (_isFavorited) ? brandYellow : brandWhite,
-                  ),
-                ),
-                top: 20,
-                right: 20,
-              ),
-            ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => ApartmentDetailsScreen(),
           ),
-          SizedBox(
-            height: 10.05,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'Marvin House',
-                    style: TextStyle(
-                      fontSize: 10,
-                      fontWeight: FontWeight.w600,
-                      height: 18 / 10,
+        );
+      },
+      child: Container(
+        width: double.maxFinite,
+        padding: EdgeInsets.fromLTRB(17, 14, 15, 15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: brandWhite,
+        ),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: <Widget>[
+                AspectRatio(
+                  aspectRatio: 300 / 218.6,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: Image(
+                      image: AssetImage('lib/assets/images/apartment_pic.png'),
+                      fit: BoxFit.fitWidth,
                     ),
                   ),
-                  Text(
-                    'Lagos, Nigeria',
+                ),
+                Positioned(
+                  top: 15,
+                  left: 12,
+                  child: Container(
+                    height: 27,
+                    width: 47.5,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24),
+                      color: Color(0x20ffffff),
+                    ),
+                    child: Stack(
+                      alignment: AlignmentDirectional.center,
+                      children: [
+                        ImageFiltered(
+                          imageFilter: ImageFilter.blur(sigmaX: 3.0),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            ImageIcon(
+                              AssetImage('lib/assets/images/rating_icon.png'),
+                              size: 18,
+                              color: brandYellow,
+                            ),
+                            Text(
+                              '4.0',
+                              style: TextStyle(
+                                fontSize: 8,
+                                fontWeight: FontWeight.w700,
+                                height: 18 / 8,
+                                color: brandWhite,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _isFavorited = !_isFavorited;
+                      });
+                    },
+                    child: ImageIcon(
+                      AssetImage('lib/assets/images/favorite_icon.png'),
+                      color: (_isFavorited) ? brandYellow : brandWhite,
+                    ),
+                  ),
+                  top: 20,
+                  right: 20,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10.05,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Marvin House',
+                      style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.w600,
+                        height: 18 / 10,
+                      ),
+                    ),
+                    Text(
+                      'Lagos, Nigeria',
+                      style: TextStyle(
+                        fontSize: 8,
+                        height: 18 / 8,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    FlatIconButton(
+                      iconPath: 'lib/assets/images/bedroom_icon.png',
+                      text: '3 Bedroom',
+                    ),
+                    SizedBox(
+                      width: 11.8,
+                    ),
+                    FlatIconButton(
+                      iconPath: 'lib/assets/images/wifi_icon.png',
+                      text: 'Wifi',
+                    ),
+                    SizedBox(
+                      width: 11.8,
+                    ),
+                    Container(
+                      height: 18,
+                      width: 18,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: brandOrange,
+                      ),
+                      child: ImageIcon(
+                        AssetImage('lib/assets/images/arrow_icon.png'),
+                        color: brandWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 5.73,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                RichText(
+                  text: TextSpan(
                     style: TextStyle(
                       fontSize: 8,
                       height: 18 / 8,
+                      color: Color(0xffa1a0a6),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  FlatIconButton(
-                    iconPath: 'lib/assets/images/bedroom_icon.png',
-                    text: '3 Bedroom',
-                  ),
-                  SizedBox(
-                    width: 11.8,
-                  ),
-                  FlatIconButton(
-                    iconPath: 'lib/assets/images/wifi_icon.png',
-                    text: 'Wifi',
-                  ),
-                  SizedBox(
-                    width: 11.8,
-                  ),
-                  Container(
-                    height: 18,
-                    width: 18,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: brandOrange,
-                    ),
-                    child: ImageIcon(
-                      AssetImage('lib/assets/images/arrow_icon.png'),
-                      color: brandWhite,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 5.73,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              RichText(
-                text: TextSpan(
-                  style: TextStyle(
-                    fontSize: 8,
-                    height: 18 / 8,
-                    color: Color(0xffa1a0a6),
-                  ),
-                  children: [
-                    TextSpan(
-                      text: '\$230 ',
-                      style: TextStyle(
-                        color: brandOrange,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        height: 23 / 14,
+                    children: [
+                      TextSpan(
+                        text: '\$230 ',
+                        style: TextStyle(
+                          color: brandOrange,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          height: 23 / 14,
+                        ),
                       ),
-                    ),
-                    TextSpan(text: '/ Day'),
-                  ],
-                ),
-              ),
-              Row(
-                children: <Widget>[
-                  Text(
-                    '5.0',
-                    style: TextStyle(
-                      fontSize: 10,
-                      height: 9 / 5,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Row(
-                    children: <Widget>[
-                      ImageIcon(
-                        AssetImage('lib/assets/images/rating_icon.png'),
-                        size: 20,
-                        color: brandYellow,
-                      ),
-                      ImageIcon(
-                        AssetImage('lib/assets/images/rating_icon.png'),
-                        size: 20,
-                        color: brandYellow,
-                      ),
-                      ImageIcon(
-                        AssetImage('lib/assets/images/rating_icon.png'),
-                        size: 20,
-                        color: brandYellow,
-                      ),
-                      ImageIcon(
-                        AssetImage('lib/assets/images/rating_icon.png'),
-                        size: 20,
-                        color: brandYellow,
-                      ),
-                      ImageIcon(
-                        AssetImage('lib/assets/images/rating_icon.png'),
-                        size: 20,
-                        color: brandYellow,
-                      ),
+                      TextSpan(text: '/ Day'),
                     ],
                   ),
-                ],
-              ),
-            ],
-          ),
-        ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      '5.0',
+                      style: TextStyle(
+                        fontSize: 10,
+                        height: 9 / 5,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        ImageIcon(
+                          AssetImage('lib/assets/images/rating_icon.png'),
+                          size: 20,
+                          color: brandYellow,
+                        ),
+                        ImageIcon(
+                          AssetImage('lib/assets/images/rating_icon.png'),
+                          size: 20,
+                          color: brandYellow,
+                        ),
+                        ImageIcon(
+                          AssetImage('lib/assets/images/rating_icon.png'),
+                          size: 20,
+                          color: brandYellow,
+                        ),
+                        ImageIcon(
+                          AssetImage('lib/assets/images/rating_icon.png'),
+                          size: 20,
+                          color: brandYellow,
+                        ),
+                        ImageIcon(
+                          AssetImage('lib/assets/images/rating_icon.png'),
+                          size: 20,
+                          color: brandYellow,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
