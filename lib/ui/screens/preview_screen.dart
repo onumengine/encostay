90538538dfb8012f3464eb1798408f1c4c928eaa
onumengine <onumdev@gmail.com>
2021-09-18 07:16:44 +0100

    complete the preview screen

--- lib/ui/screens/preview_screen.dart
+++ lib/ui/screens/preview_screen.dart
@@ -43,7 +43,6 @@ class _PreviewScreenState extends State<PreviewScreen> {
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 23),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               Row(
@@ -255,6 +254,135 @@ class _PreviewScreenState extends State<PreviewScreen> {
                       ],
                     ),
                   ],
+                ),
+              ),
+              SizedBox(height: screenSize.height / 39),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 30, horizontal: 24),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: brandLight,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Fee & Tax Details',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        height: 23 / 14,
+                        color: lightTextColor,
+                      ),
+                    ),
+                    SizedBox(height: screenSize.height / 36.9),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          '\$172 x 3 nights',
+                          style: TextStyle(
+                            fontSize: 12,
+                            height: 22 / 12,
+                            color: lightTextColor,
+                          ),
+                        ),
+                        Text(
+                          '\$516',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            height: 20 / 12,
+                            color: lightTextColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: screenSize.height / 54.13),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          'Service charges',
+                          style: TextStyle(
+                            fontSize: 12,
+                            height: 22 / 12,
+                            color: lightTextColor,
+                          ),
+                        ),
+                        Text(
+                          '\$15',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            height: 20 / 12,
+                            color: lightTextColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: screenSize.height / 34.55),
+                    Divider(
+                      color: brandYellow,
+                      thickness: 1,
+                    ),
+                    SizedBox(height: screenSize.height / 36.09),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          'Total',
+                          style: TextStyle(
+                            height: 25 / 14,
+                          ),
+                        ),
+                        PhysicalModel(
+                          color: Color(0x7000000),
+                          shadowColor: brandBrown,
+                          elevation: 3,
+                          borderRadius: BorderRadius.circular(7),
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 22),
+                            decoration: BoxDecoration(
+                              color: brandWhite,
+                              borderRadius: BorderRadius.circular(7),
+                            ),
+                            child: Text(
+                              '\$524',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                                height: 30 / 18,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: screenSize.height / 7.06,
+              ),
+              Container(
+                constraints: BoxConstraints(maxWidth: screenSize.width / 2.5),
+                padding: EdgeInsets.symmetric(vertical: 17, horizontal: 24),
+                decoration: BoxDecoration(
+                  color: brandBrown,
+                  borderRadius: BorderRadius.circular(26.5),
+                ),
+                child: Center(
+                  child: Text(
+                    'Reserve',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      height: 19.5 / 16,
+                      color: brandWhite,
+                    ),
+                  ),
                 ),
               ),
             ],
