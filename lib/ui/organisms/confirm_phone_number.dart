8270804efc357d39e0e442d50ed734870bbcbfec
onumengine <onumdev@gmail.com>
2021-09-19 13:25:16 +0100

    create the confirm phone number component

---
+++ lib/ui/organisms/confirm_phone_number.dart
@@ -0,0 +1,91 @@
+import 'package:encostay/utilities/colors.dart';
+import 'package:encostay/utilities/constants.dart';
+import 'package:flutter/material.dart';
+
+class ConfirmPhoneNumberComponent extends StatefulWidget {
+  const ConfirmPhoneNumberComponent({Key? key}) : super(key: key);
+
+  @override
+  _ConfirmPhoneNumberComponentState createState() =>
+      _ConfirmPhoneNumberComponentState();
+}
+
+class _ConfirmPhoneNumberComponentState
+    extends State<ConfirmPhoneNumberComponent> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 42,
+          ),
+          Align(
+            alignment: AlignmentDirectional.centerStart,
+            child: Text(
+              'Confirm your phone number',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+                height: 34 / 24,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Container(
+            padding: EdgeInsets.all(24) + EdgeInsets.only(right: 66),
+            decoration: BoxDecoration(
+              color: brandWhite,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 3),
+                  blurRadius: 15,
+                  color: Color(0x7000000),
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Phone Number',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                    height: 20 / 12,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Image(
+                      image: AssetImage(
+                          ASSET_IMAGE_PATH + 'nigeria_flag_icon.png'),
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: '070x xxx xxxx',
+                        ),
+                        keyboardType: TextInputType.phone,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
