36cbfdf2736927811fcdac3dd9c0940d5d353e34
onumengine <onumdev@gmail.com>
2021-09-20 16:01:01 +0100

    add new asset directories to the project

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,6 +1,10 @@
 const AMENITY_ICONS_PATH = 'lib/assets/images/amenity_icons/amenity_ic_';
 const ASSET_IMAGE_PATH = 'lib/assets/images/';
 
+const COLORED_IMAGES_PATH = 'lib/assets/images/colored/';
+const ORANGE_IMAGES_PATH = 'lib/assets/images/orange/';
+const IMAGES_PATH = 'lib/assets/images/images/';
+
 const COLORED_VECTORS_PATH = 'lib/assets/vectors/colored_icons/';
 const ORANGE_VECTORS_PATH = 'lib/assets/vectors/orange_icons/';
 const VECTOR_IMAGES_PATH = 'lib/assets/vectors/images/';
