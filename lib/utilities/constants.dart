f21e0523804431a7271895279ab9da4d6da247b9
onumengine <onumdev@gmail.com>
2021-09-20 15:01:57 +0100

    add new vector assets

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,2 +1,6 @@
 const AMENITY_ICONS_PATH = 'lib/assets/images/amenity_icons/amenity_ic_';
 const ASSET_IMAGE_PATH = 'lib/assets/images/';
+
+const COLORED_VECTORS_PATH = 'lib/assets/vectors/colored_icons/';
+const ORANGE_VECTORS_PATH = 'lib/assets/vectors/orange_icons/';
+const VECTOR_IMAGES_PATH = 'lib/assets/vectors/images/';
