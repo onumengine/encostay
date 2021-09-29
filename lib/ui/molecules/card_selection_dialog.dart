cae34ca5488b539eee08ca863828fdbe72f12431
onumengine <onumdev@gmail.com>
2021-09-29 22:04:10 +0100

    add a list of PaymenTcardTile widgets to the CardSelectionDialog

--- lib/ui/molecules/card_selection_dialog.dart
+++ lib/ui/molecules/card_selection_dialog.dart
@@ -1,10 +1,43 @@
+import 'package:encostay/models/payment_card.dart';
+import 'package:encostay/ui/molecules/payment_card_tile.dart';
+import 'package:encostay/utilities/enums.dart';
 import 'package:flutter/material.dart';
 
 class CardSelectionDialog extends StatelessWidget {
-  const CardSelectionDialog({Key? key}) : super(key: key);
+  CardSelectionDialog({Key? key}) : super(key: key);
+
+  final List<PaymentCard> availableCards = <PaymentCard>[
+    DebitCard(
+      name: 'John Doe Richdie',
+      cardNumber: '125412541254',
+      expiryDate: '10/24',
+      cvv: '258',
+      paymentProcessor: PaymentProcessor.mastercard,
+    ),
+    DebitCard(
+      name: 'John Doe Richdie',
+      cardNumber: '125412541254',
+      expiryDate: '10/24',
+      cvv: '258',
+      paymentProcessor: PaymentProcessor.mastercard,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        PaymentCardTile(
+          card: availableCards[0],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        PaymentCardTile(
+          card: availableCards[1],
+        ),
+        SizedBox(height: 20)
+      ],
+    );
   }
 }
