e92972d5d5aa9fddac80b8f6142bf97b4ace22fb
onumengine <onumdev@gmail.com>
2021-10-04 18:51:44 +0100

    create named routes for all pages in app

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,3 +1,5 @@
+/* ------------------ IMAGES ------------------*/
+
 const AMENITY_ICONS_PATH = 'lib/assets/images/amenity_icons/amenity_ic_';
 const ASSET_IMAGE_PATH = 'lib/assets/images/';
 
@@ -9,3 +11,20 @@ const VECTORS_ROOT_PATH = 'lib/assets/vectors/';
 const COLORED_VECTORS_PATH = 'lib/assets/vectors/colored_icons/';
 const ORANGE_VECTORS_PATH = 'lib/assets/vectors/orange_icons/';
 const VECTOR_IMAGES_PATH = 'lib/assets/vectors/images/';
+
+/* ------------------ ROUTES ------------------*/
+
+const ROUTE_ADD_CARD = '/add_card';
+const ROUTE_ADD_PAYMENT = '/add_payment';
+const ROUTE_APARTMENT_DETAILS = '/apartment_details/';
+const ROUTE_AUTH_SCREEN = '/auth';
+const ROUTE_BOOKING = '/booking';
+const ROUTE_CANCELLATION = '/cancel';
+const ROUTE_FILTERS = '/filters';
+const ROUTE_HOME = '/home';
+const ROUTE_ONBOARDING = '/onboarding';
+const ROUTE_OWNER = '/owner';
+const ROUTE_PREVIEW = '/preview';
+const ROUTE_SEARCH_RESULTS = '/search_results';
+const ROUTE_SET_PASSWORD = '/set_password';
+const ROUTE_SPLASH = '/splash';
