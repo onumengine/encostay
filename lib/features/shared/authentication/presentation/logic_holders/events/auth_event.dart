c9c5a831ec10db9b1ca96996183f3e292995d0b9
onumengine <onumdev@gmail.com>
2021-10-30 18:36:40 +0100

    create an event for selecting the user account type in the bloc

--- lib/features/shared/authentication/presentation/logic_holders/events/auth_event.dart
+++ lib/features/shared/authentication/presentation/logic_holders/events/auth_event.dart
@@ -1,6 +1,16 @@
+import 'package:encostay/core/utilities/enums.dart';
 import 'package:equatable/equatable.dart';
 
 abstract class AuthEvent extends Equatable {
   @override
   List<Object?> get props => [];
+}
+
+class SelectAccountType extends AuthEvent {
+  final AccountType accountType;
+
+  SelectAccountType({required this.accountType});
+
+  @override
+  List<Object?> get props => [accountType];
 }
