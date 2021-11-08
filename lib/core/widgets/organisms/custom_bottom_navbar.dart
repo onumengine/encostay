89da14693881944b482788803ff1a79333d7c195
onumengine <onumdev@gmail.com>
2021-11-08 16:26:17 +0100

    complete the CustomBottomNavbar

---
+++ lib/core/widgets/organisms/custom_bottom_navbar.dart
@@ -0,0 +1,70 @@
+import 'package:encostay/core/utilities/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class CustomBottomNavbar extends StatelessWidget {
+  int currentIndex;
+  void Function(int selectedindex) onTap;
+
+  CustomBottomNavbar({
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 57,
+      padding: EdgeInsets.symmetric(vertical: 11, horizontal: 14),
+      margin:
+          EdgeInsets.symmetric(horizontal: 29) + EdgeInsets.only(bottom: 42),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(70),
+        color: brandBrown,
+      ),
+      child: GNav(
+        tabs: [
+          GButton(
+            icon: Icons.home_outlined,
+            iconColor: brandWhite,
+            text: 'Home',
+            textColor: brandBrown,
+            backgroundColor: brandWhite,
+            gap: 16,
+            padding: EdgeInsets.symmetric(vertical: 9, horizontal: 16),
+          ),
+          GButton(
+            icon: Icons.filter_none_outlined,
+            iconColor: brandWhite,
+            text: 'Bookings',
+            textColor: brandBrown,
+            backgroundColor: brandWhite,
+            gap: 16,
+            padding: EdgeInsets.symmetric(vertical: 9, horizontal: 16),
+          ),
+          GButton(
+            icon: Icons.near_me_outlined,
+            iconColor: brandWhite,
+            text: 'Location',
+            textColor: brandBrown,
+            backgroundColor: brandWhite,
+            gap: 16,
+            padding: EdgeInsets.symmetric(vertical: 9, horizontal: 16),
+          ),
+          GButton(
+            icon: Icons.person_outline_outlined,
+            iconColor: brandWhite,
+            text: 'Profile',
+            textColor: brandBrown,
+            backgroundColor: brandWhite,
+            gap: 16,
+            padding: EdgeInsets.symmetric(vertical: 9, horizontal: 16),
+          ),
+        ],
+        onTabChange: onTap,
+        tabBorderRadius: 70,
+        color: brandBrown,
+      ),
+    );
+  }
+}
--- lib/features/host/leasing/presentation/ui/screens/home_screen.dart
+++ lib/features/host/leasing/presentation/ui/screens/home_screen.dart
@@ -1,4 +1,8 @@
+import 'package:encostay/core/utilities/colors.dart';
+import 'package:encostay/core/utilities/constants.dart';
+import 'package:encostay/core/widgets/organisms/custom_bottom_navbar.dart';
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
 class HostHomeScreen extends StatefulWidget {
   const HostHomeScreen({Key? key}) : super(key: key);
@@ -8,25 +12,102 @@ class HostHomeScreen extends StatefulWidget {
 }
 
 class _HostHomeScreenState extends State<HostHomeScreen> {
-  late final List<StatelessWidget> _components;
-  late int _currentComponentIndex;
+  late final List<StatelessWidget> components;
+  late final List<PersistentBottomNavBarItem> persistentNavbarItems;
+  late int _currentScreenIndex;
+  late PersistentTabController persistentTabController;
 
   @override
   void initState() {
     super.initState();
-    _components = const [
+    components = const [
       HomeComponent(),
       BookingsViewComponent(),
       ChatComponent(),
       HostProfileComponent(),
     ];
-    _currentComponentIndex = 0;
+    persistentNavbarItems = <PersistentBottomNavBarItem>[
+      PersistentBottomNavBarItem(
+        icon: ImageIcon(
+          AssetImage(ASSET_IMAGE_PATH + 'navbar_home_ic.png'),
+          color: brandBrown,
+        ),
+        inactiveIcon: ImageIcon(
+          AssetImage(ASSET_IMAGE_PATH + 'navbar_home_ic.png'),
+          color: brandWhite,
+        ),
+        title: 'Home',
+        activeColorPrimary: brandWhite,
+        activeColorSecondary: brandBrown,
+      ),
+      PersistentBottomNavBarItem(
+        icon: ImageIcon(
+          AssetImage(ASSET_IMAGE_PATH + 'navbar_book_ic.png'),
+          color: brandBrown,
+        ),
+        inactiveIcon: ImageIcon(
+          AssetImage(ASSET_IMAGE_PATH + 'navbar_book_ic.png'),
+          color: brandWhite,
+        ),
+        title: 'Bookings',
+        activeColorPrimary: brandWhite,
+        activeColorSecondary: brandBrown,
+      ),
+      PersistentBottomNavBarItem(
+        icon: ImageIcon(
+          AssetImage(ASSET_IMAGE_PATH + 'navbar_location_ic.png'),
+          color: brandBrown,
+        ),
+        inactiveIcon: ImageIcon(
+          AssetImage(ASSET_IMAGE_PATH + 'navbar_location_ic.png'),
+          color: brandWhite,
+        ),
+        title: 'Chat',
+        activeColorPrimary: brandWhite,
+        activeColorSecondary: brandBrown,
+      ),
+      PersistentBottomNavBarItem(
+        icon: ImageIcon(
+          AssetImage(ASSET_IMAGE_PATH + 'navbar_user_ic.png'),
+          color: brandBrown,
+        ),
+        inactiveIcon: ImageIcon(
+          AssetImage(ASSET_IMAGE_PATH + 'navbar_user_ic.png'),
+          color: brandWhite,
+        ),
+        title: 'Profile',
+        activeColorPrimary: brandWhite,
+        activeColorSecondary: brandBrown,
+      ),
+    ];
+    _currentScreenIndex = 0;
+    persistentTabController = PersistentTabController(initialIndex: 0);
   }
+
+  /*
+  @override
+  Widget build(BuildContext context) {
+    return PersistentTabView(
+      context,
+      controller: persistentTabController,
+      screens: components,
+      items: persistentNavbarItems,
+      backgroundColor: brandBrown,
+      navBarStyle: NavBarStyle.style7,
+    );
+  }
+  */
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _components.elementAt(_currentComponentIndex),
+      body: components.elementAt(_currentScreenIndex),
+      bottomNavigationBar: CustomBottomNavbar(
+        currentIndex: 0,
+        onTap: (index) {
+          print('selected index $index');
+        },
+      ),
     );
   }
 }
