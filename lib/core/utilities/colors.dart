eb391c0e1ec251b936b3fbbf67031093665f23cb
onumengine <onumdev@gmail.com>
2021-11-10 22:57:04 +0100

    create a ColorPalette class

--- lib/core/utilities/colors.dart
+++ lib/core/utilities/colors.dart
@@ -36,3 +36,43 @@ const googleWhite = Color(0xfffafafa);
 const hamburgerIconColor = Color(0xff774037);
 const brownTextColor = Color(0xff6c2c00);
 const palehostText = Color(0xff9b9b9b);
+
+abstract class ColorPalette {
+  static const brandOrange = Color(0xfffea401);
+  static const brandBrown = Color(0xff412525);
+  static const brandYellow = Color(0xfffece00);
+  static const brandLight = Color(0xfffff4e0);
+  static const brandLightGreen = Color(0xffe2f4f3);
+  static const brandPaleGreen = Color(0xfff7fbf9);
+  static const brandDarkGreen = Color(0xff05837f);
+  static const brandWhite = Color(0xffffffff);
+  static const brandBackground = Color(0xfffafafa);
+  static const brandBackgroundApp = Color(0xfff2f2f2);
+  static const brandSecondBackground = Color(0xfff8f8f8);
+  static const brandOverlay = Color(0x43ffffff);
+  static const brandGrey = Color(0xffa9a9a9);
+  static const brandGreyAlt = Color(0xff3e3e3e);
+  static const brandGreyAltOp80 = Color(0x503e3e3e);
+  static const brandTextInputShadow = Color(0xff939393);
+
+  static const lightTextColor = Color(0xff2f2f2F);
+  static const paleTextColor = Color(0xffa1a0a6);
+
+  static const redTextColor = Color(0Xffff765e);
+  static const redBgColor = Color(0xfffff4f1);
+
+  static const greyCardColor = Color(0Xfff8f9f9);
+
+  // Modal colors
+  static const brandBrownDark = Color(0xff313131);
+  static const instructionalTextModal = Color(0xff616161);
+
+  static const facebookBlue = Color(0xff486ab1);
+  static const googleWhite = Color(0xfffafafa);
+
+  // Host Colors
+  static const hamburgerIconColor = Color(0xff774037);
+  static const brownTextColor = Color(0xff6c2c00);
+  static const palehostText = Color(0xff9b9b9b);
+  static const paleHostGrey = Color(0xff787878);
+}
