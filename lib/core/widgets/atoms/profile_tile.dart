13f31184ff9f96ba9eea832a018351cba6dcd682
onumengine <onumdev@gmail.com>
2021-11-11 15:18:55 +0100

    finish the profile screen

---
+++ lib/core/widgets/atoms/profile_tile.dart
@@ -0,0 +1,42 @@
+import 'package:encostay/core/utilities/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class ProfileItemTile extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+  const ProfileItemTile({Key? key, required this.title, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        print('tapped $title');
+      },
+      child: Material(
+        color: Colors.transparent,
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.08,
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.symmetric(horizontal: 20) +
+              EdgeInsets.only(bottom: 10),
+          padding: EdgeInsets.all(21),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SvgPicture.asset(ORANGE_VECTORS_PATH + 'ic_orange_dot.svg'),
+              SizedBox(
+                width: 12,
+              ),
+              Text(title),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/host/leasing/presentation/ui/components/host_profile_component.dart
@@ -0,0 +1,87 @@
+import 'package:encostay/core/utilities/colors.dart';
+import 'package:encostay/core/utilities/constants.dart';
+import 'package:encostay/core/widgets/atoms/profile_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HostProfileComponent extends StatelessWidget {
+  const HostProfileComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          automaticallyImplyLeading: true,
+          backgroundColor: Colors.transparent,
+          foregroundColor: ColorPalette.brandBrown,
+          centerTitle: true,
+          title: Text(
+            'Profile',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              height: 30 / 18,
+            ),
+          ),
+        ),
+        SliverFillRemaining(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(25),
+                  child: SvgPicture.asset(
+                    VECTOR_IMAGES_PATH + 'owner.svg',
+                    height: 90,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 21),
+                  child: Text(
+                    'Robert Bowie',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                      height: 34 / 24,
+                      color: ColorPalette.lightTextColor,
+                    ),
+                  ),
+                ),
+                ProfileItemTile(
+                  title: 'Edit Profile',
+                  onTap: () {},
+                ),
+                ProfileItemTile(
+                  title: 'Change Password',
+                  onTap: () {},
+                ),
+                ProfileItemTile(
+                  title: 'Payment Method',
+                  onTap: () {},
+                ),
+                ProfileItemTile(
+                  title: 'Transactions',
+                  onTap: () {},
+                ),
+                ProfileItemTile(
+                  title: 'My Listings',
+                  onTap: () {},
+                ),
+                ProfileItemTile(
+                  title: 'Contact Support',
+                  onTap: () {},
+                ),
+                ProfileItemTile(
+                  title: 'Log Out',
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
