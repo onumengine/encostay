ee3faaab425bc79307fa2871fab237711190ccde
onumengine <onumdev@gmail.com>
2021-11-17 22:33:16 +0100

    rename the transaction logic holders

--- lib/features/host/tx_management/presentation/logic_holders/bloc/transaction_bloc.dart
+++ lib/features/host/tx_management/presentation/logic_holders/bloc/transaction_bloc.dart
@@ -1,5 +1,5 @@
-import 'package:encostay/features/host/tx_management/presentation/logic_holders/event/transaction_history_event.dart';
-import 'package:encostay/features/host/tx_management/presentation/logic_holders/state/transaction_history_state.dart';
+import 'package:encostay/features/host/tx_management/presentation/logic_holders/event/transaction_event.dart';
+import 'package:encostay/features/host/tx_management/presentation/logic_holders/state/transaction_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TransactionsBloc extends Bloc<TransactionEvent, TransactionState> {
