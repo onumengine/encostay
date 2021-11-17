5434e621a54d8ab25c94a5659a9e0e82cae65d88
onumengine <onumdev@gmail.com>
2021-11-17 22:26:41 +0100

    create the TransactionHistoryScreen

---
+++ lib/features/host/tx_management/presentation/ui/screens/transaction_history_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TransactionHistoryScreen extends StatelessWidget {
+  const TransactionHistoryScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
