7102c70f4358264cfb95b7cc0e27b27c9af5845c
onumengine <onumdev@gmail.com>
2021-11-25 14:32:03 +0100

    return a ConnectionFailure if the device is not connected

--- lib/features/shared/sign_up/data/repositories/SubmitSignupFormRepoImpl.dart
+++ lib/features/shared/sign_up/data/repositories/SubmitSignupFormRepoImpl.dart
@@ -1,3 +1,4 @@
+import 'package:encostay/core/network/NetworkInfo.dart';
 import 'package:encostay/features/shared/sign_up/data/data_sources/RemoteDataSource.dart';
 import 'package:encostay/features/shared/sign_up/domain/entities/UserCredentialEntity.dart';
 import 'package:encostay/core/error/Failure.dart';
@@ -6,18 +7,26 @@ import 'package:encostay/features/shared/sign_up/domain/repositories/SubmitSignu
 
 class SubmitSignupFormRepoImpl implements SubmitSignupFormRepo {
   final RemoteDataSource remoteDataSource;
+  final NetworkInfo connectionChecker;
 
-  SubmitSignupFormRepoImpl({required this.remoteDataSource});
+  SubmitSignupFormRepoImpl({
+    required this.remoteDataSource,
+    required this.connectionChecker,
+  });
 
   @override
   Future<Either<Failure, UserCredentialEntity>> submitForm(
       Map<String, dynamic> formData) async {
-    try {
-      final UserCredentialEntity credential =
-          await remoteDataSource.submitForm(formData);
-      return Right(credential);
-    } on Exception catch (e) {
-      return Left(SignupFailure(message: e.toString()));
+    if (await connectionChecker.deviceIsConnected) {
+      try {
+        final UserCredentialEntity credential =
+            await remoteDataSource.submitForm(formData);
+        return Right(credential);
+      } on Exception catch (e) {
+        return Left(SignupFailure(message: e.toString()));
+      }
+    } else {
+      return Left(ConnectionFailure());
     }
   }
 }
