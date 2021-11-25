aa7a4bf47d0c3841ad2836ac6f9038cc7761fbf1
onumengine <onumdev@gmail.com>
2021-11-25 14:13:35 +0100

    create a Failure class for Signup errors

--- lib/core/error/Failure.dart
+++ lib/core/error/Failure.dart
@@ -4,3 +4,12 @@ abstract class Failure extends Equatable {
   @override
   List<Object?> get props => [];
 }
+
+class SignupFailure extends Failure {
+  final String message;
+
+  SignupFailure({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+}
