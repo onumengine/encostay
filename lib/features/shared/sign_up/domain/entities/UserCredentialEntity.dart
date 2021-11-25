380c3745c4743bf696a5f998eab271bbd3374d58
onumengine <onumdev@gmail.com>
2021-11-25 11:08:02 +0100

    create a UserCredential entity

--- lib/features/shared/sign_up/domain/entities/UserCredentialEntity.dart
+++ lib/features/shared/sign_up/domain/entities/UserCredentialEntity.dart
@@ -1,6 +1,15 @@
+import 'package:encostay/core/utilities/enums.dart';
 import 'package:equatable/equatable.dart';
 
 class UserCredentialEntity extends Equatable {
+  final String userID;
+  final AccountType accountType;
+
+  UserCredentialEntity({
+    required this.userID,
+    required this.accountType,
+  });
+
   @override
   List<Object?> get props => [];
 }
