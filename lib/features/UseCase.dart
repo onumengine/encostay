3dbc6a5fbf93dc32e914232689dc5daf78fde81e
onumengine <onumdev@gmail.com>
2021-11-25 11:06:26 +0100

    create a base UseCase interface for app use cases

--- lib/features/UseCase.dart
+++ lib/features/UseCase.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:encostay/core/error/Failure.dart';
 
-abstract class UseCase<Type> {
-  Future<Either<Failure, Type>> call();
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
 }
