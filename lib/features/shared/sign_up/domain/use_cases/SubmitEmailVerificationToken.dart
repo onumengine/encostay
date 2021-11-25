e9e31e1864337ab8f28cbb07b717375b05279813
onumengine <onumdev@gmail.com>
2021-11-25 11:05:53 +0100

    create a SubmitEmailVerificationToke UseCase

--- lib/features/shared/sign_up/domain/use_cases/SubmitEmailVerificationToken.dart
+++ lib/features/shared/sign_up/domain/use_cases/SubmitEmailVerificationToken.dart
@@ -1,13 +1,30 @@
+// TODO: Create the SendEmailVerificationToken UseCase
+
+/*
 import 'package:encostay/core/error/Failure.dart';
 import 'package:dartz/dartz.dart';
+import 'package:encostay/features/Params.dart';
 import 'package:encostay/features/UseCase.dart';
 import 'package:encostay/features/shared/sign_up/domain/entities/VerificationStatusEntity.dart';
 
 class SubmitEmailVerificationToken
-    implements UseCase<VerificationStatusEntity> {
+    implements UseCase<VerificationStatusEntity, Token> {
+  final SubmitVerificationTokenRepo repository;
+
+  SubmitEmailVerificationToken({required this.repository});
+
   @override
-  Future<Either<Failure, VerificationStatusEntity>> call() {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<Either<Failure, VerificationStatusEntity>> call(Token token) async {
+    return await repository.submitToken(token.number);
   }
 }
+
+class Token extends Params {
+  final int number;
+
+  Token({required this.number});
+
+  @override
+  List<Object?> get props => [number];
+}
+*/
