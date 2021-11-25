6151a65956b129573d779869aea0a1d59d49324e
onumengine <onumdev@gmail.com>
2021-11-25 11:05:23 +0100

    create a SubmitSignupForm UseCase

--- lib/features/shared/sign_up/domain/use_cases/SubmitSignupForm.dart
+++ lib/features/shared/sign_up/domain/use_cases/SubmitSignupForm.dart
@@ -1,12 +1,26 @@
 import 'package:encostay/core/error/Failure.dart';
 import 'package:dartz/dartz.dart';
+import 'package:encostay/features/Params.dart';
 import 'package:encostay/features/UseCase.dart';
 import 'package:encostay/features/shared/sign_up/domain/entities/UserCredentialEntity.dart';
+import 'package:encostay/features/shared/sign_up/domain/repositories/SubmitSignupFormRepo.dart';
 
-class SubmitSignupForm implements UseCase<UserCredentialEntity> {
+class SubmitSignupForm implements UseCase<UserCredentialEntity, FormParam> {
+  final SubmitSignupFormRepo repository;
+
+  SubmitSignupForm({required this.repository});
+
   @override
-  Future<Either<Failure, UserCredentialEntity>> call() {
-    // TODO: implement call method
-    throw UnimplementedError();
+  Future<Either<Failure, UserCredentialEntity>> call(FormParam form) async {
+    return await repository.submitForm(form.data);
   }
+}
+
+class FormParam extends Params {
+  final Map data;
+
+  FormParam({required this.data});
+
+  @override
+  List<Object?> get props => [data];
 }
