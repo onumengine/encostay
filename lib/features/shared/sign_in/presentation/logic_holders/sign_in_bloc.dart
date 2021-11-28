5de2b5d187fd27f4faab5a9f92921b01fb82557e
onumengine <onumdev@gmail.com>
2021-11-28 05:17:33 +0100

    handle sign in in the bloc

--- lib/features/shared/sign_in/presentation/logic_holders/sign_in_bloc.dart
+++ lib/features/shared/sign_in/presentation/logic_holders/sign_in_bloc.dart
@@ -1,13 +1,28 @@
+import 'package:encostay/features/shared/sign_in/domain/use_cases/LoginWiithEmail.dart';
 import 'package:encostay/features/shared/sign_in/presentation/logic_holders/sign_in_event.dart';
 import 'package:encostay/features/shared/sign_in/presentation/logic_holders/sign_in_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignInBloc extends Bloc<SignInEvent, SignInState> {
-  SignInBloc() : super(UnsignedIn());
+  final LoginWithEmail loginWithEmail;
+  final Map<String, String> loginData = {};
+
+  SignInBloc({required this.loginWithEmail}) : super(DefaultSignInState());
 
   @override
   Stream<SignInState> mapEventToState(SignInEvent event) async* {
-    // TODO: implement mapEventToState
-    throw UnimplementedError();
+    if (event is SubmitSignInForm) {
+      yield SigningIn();
+      updateLoginData(event);
+      final result = await loginWithEmail(LoginParams(loginData: loginData));
+      yield result.fold(
+          (failure) => SignInFailed(), (loginCredential) => SignInComplete());
+    }
+  }
+
+  void updateLoginData(SubmitSignInForm event) {
+    loginData['email'] = event.email;
+    loginData['password'] = event.password;
+    print('LOGIN DATA: $loginData');
   }
 }
