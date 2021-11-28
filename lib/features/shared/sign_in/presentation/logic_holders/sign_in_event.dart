9ea07e15595780f4d2c4bd12791be5aa834446f9
onumengine <onumdev@gmail.com>
2021-11-28 05:16:44 +0100

    create an event for triggering login/sign up

--- lib/features/shared/sign_in/presentation/logic_holders/sign_in_event.dart
+++ lib/features/shared/sign_in/presentation/logic_holders/sign_in_event.dart
@@ -4,3 +4,18 @@ abstract class SignInEvent extends Equatable {
   @override
   List<Object?> get props => [];
 }
+
+class SubmitSignInForm extends SignInEvent {
+  final String email, password;
+
+  SubmitSignInForm({
+    required this.email,
+    required this.password,
+  });
+
+  @override
+  List<Object?> get props => [
+        email,
+        password,
+      ];
+}
