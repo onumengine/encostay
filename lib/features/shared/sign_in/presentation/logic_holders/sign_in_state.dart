5f582e9da7f79768871e8b9fd13b81761cad7636
onumengine <onumdev@gmail.com>
2021-11-28 05:17:03 +0100

    create different sign in states

--- lib/features/shared/sign_in/presentation/logic_holders/sign_in_state.dart
+++ lib/features/shared/sign_in/presentation/logic_holders/sign_in_state.dart
@@ -5,4 +5,10 @@ abstract class SignInState extends Equatable {
   List<Object?> get props => [];
 }
 
-class UnsignedIn extends SignInState {}
+class DefaultSignInState extends SignInState {}
+
+class SigningIn extends SignInState {}
+
+class SignInComplete extends SignInState {}
+
+class SignInFailed extends SignInState {}
