4e921d5fbed1e3b6496600f9557d1483220b7516
onumengine <onumdev@gmail.com>
2021-11-28 02:56:20 +0100

    make the LoginParams take a map

--- lib/features/shared/sign_in/domain/use_cases/LoginWiithEmail.dart
+++ lib/features/shared/sign_in/domain/use_cases/LoginWiithEmail.dart
@@ -3,26 +3,27 @@ import 'package:dartz/dartz.dart';
 import 'package:encostay/features/Params.dart';
 import 'package:encostay/features/UseCase.dart';
 import 'package:encostay/features/shared/sign_in/domain/entities/LoginCredential.dart';
+import 'package:encostay/features/shared/sign_in/domain/repositories/EmailLoginRepo.dart';
 
-class LoginWithEmail implements UseCase<LoginCredential, LoginParams> {
+class LoginWithEmail implements UseCase<LoginCredentialEntity, LoginParams> {
+  final EmailLoginRepo repository;
+
+  LoginWithEmail({required this.repository});
+
   @override
-  Future<Either<Failure, LoginCredential>> call(LoginParams params) {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<Either<Failure, LoginCredentialEntity>> call(
+      LoginParams params) async {
+    return await repository.loginWithEmail(params.loginData);
   }
 }
 
 class LoginParams extends Params {
-  final String email, password;
+  final Map<String, String> loginData;
 
   LoginParams({
-    required this.email,
-    required this.password,
+    required this.loginData,
   });
 
   @override
-  List<Object?> get props => [
-        email,
-        password,
-      ];
+  List<Object?> get props => [loginData];
 }
