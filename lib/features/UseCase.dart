54d53d74f2b9c9788b2eb861e1494cb0db72d065
onumengine <onumdev@gmail.com>
2021-12-08 13:39:17 +0100

    make the UseCase generics more explanatory

--- lib/features/UseCase.dart
+++ lib/features/UseCase.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:encostay/core/error/Failure.dart';
 
-abstract class UseCase<Type, Params> {
-  Future<Either<Failure, Type>> call(Params params);
+abstract class UseCase<EntityType, Params> {
+  Future<Either<Failure, EntityType>> call(Params params);
 }
