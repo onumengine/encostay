a406da45fba3c22ebddefbca87d0af64987e91e6
onumengine <onumdev@gmail.com>
2021-12-09 10:47:10 +0100

    correct the use case definition syntax error

--- lib/features/shared/onboarding/domain/use_cases/check_first_launch.dart
+++ lib/features/shared/onboarding/domain/use_cases/check_first_launch.dart
@@ -4,9 +4,9 @@ import 'package:encostay/features/Params.dart';
 import 'package:encostay/features/UseCase.dart';
 import 'package:encostay/features/shared/onboarding/domain/entities/launch_status_entity.dart';
 
-class CheckFirstLaunch extends UseCase<NoParams, LaunchStatusEntity> {
+class CheckFirstLaunch extends UseCase<LaunchStatusEntity, NoParams> {
   @override
-  Future<Either<Failure, NoParams>> call(LaunchStatusEntity params) {
+  Future<Either<Failure, LaunchStatusEntity>> call(NoParams params) {
     // TODO: implement call
     throw UnimplementedError();
   }
