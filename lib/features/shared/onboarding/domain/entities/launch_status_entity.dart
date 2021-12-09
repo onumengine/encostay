c91abd5bc856ebe69906797e8e6b6f23e3562061
onumengine <onumdev@gmail.com>
2021-12-09 11:02:38 +0100

    add a field to hold the launch status

--- lib/features/shared/onboarding/domain/entities/launch_status_entity.dart
+++ lib/features/shared/onboarding/domain/entities/launch_status_entity.dart
@@ -1,6 +1,10 @@
 import 'package:equatable/equatable.dart';
 
 class LaunchStatusEntity extends Equatable {
+  final bool isFirstLaunch;
+
+  LaunchStatusEntity({required this.isFirstLaunch});
+
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [isFirstLaunch];
 }
