dfc3c70d1dd731c68c0cd67833884e53932a3dc9
onumengine <onumdev@gmail.com>
2021-12-12 17:02:34 +0100

    create a use case for completing onboarding

--- lib/features/shared/onboarding/domain/use_cases/activate_onboarded_status.dart
+++ lib/features/shared/onboarding/domain/use_cases/activate_onboarded_status.dart
@@ -3,12 +3,16 @@ import 'package:dartz/dartz.dart';
 import 'package:encostay/features/Params.dart';
 import 'package:encostay/features/UseCase.dart';
 import 'package:encostay/features/shared/onboarding/domain/entities/onboarding_status_entity.dart';
+import 'package:encostay/features/shared/onboarding/domain/repositories/onboarding_status_repo.dart';
 
 class ActivateOnboardedStatus
     implements UseCase<OnboardingStatusEntity, NoParams> {
+  final OnboardingStatusRepo repository;
+
+  ActivateOnboardedStatus({required this.repository});
+
   @override
-  Future<Either<Failure, OnboardingStatusEntity>> call(NoParams params) {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<Either<Failure, OnboardingStatusEntity>> call(NoParams params) async {
+    return repository.activateOnboardedStatus();
   }
 }
